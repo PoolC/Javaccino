077ae9bf6663527a7407f1c6ec75e4b0641eab1d
hcpak <jasotn12@naver.com>
2021-07-27 03:48:25 +0900

    Feat: Spring boot 초기 설정

---
+++ src/main/java/com/emotie/api/JavaccinoApplication.java
@@ -0,0 +1,13 @@
+package com.emotie.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaccinoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaccinoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emotie/api/JavaccinoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emotie.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaccinoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
