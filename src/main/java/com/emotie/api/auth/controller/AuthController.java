2fd0c71c7958069f3d1ea87ff2de4cb09cc7cb2a
hcpak <jasotn12@naver.com>
2021-08-04 00:13:59 +0900

    feat: Auth Controller 구현

---
+++ src/main/java/com/emotie/api/auth/controller/AuthController.java
@@ -0,0 +1,21 @@
+package com.emotie.api.auth.controller;
+
+import com.emotie.api.auth.dto.LoginRequest;
+import com.emotie.api.auth.dto.LoginResponse;
+import com.emotie.api.auth.dto.PasswordResetRequest;
+import com.emotie.api.member.domain.Member;
+import org.springframework.http.ResponseEntity;
+
+import java.util.Optional;
+
+public interface AuthController {
+    public ResponseEntity<LoginResponse> createAccessToken(LoginRequest request);
+
+    public ResponseEntity<Void> sendEmailAuthorizationToken(Member member) throws Exception;
+
+    public ResponseEntity<Void> checkAuthorizationCode(Member member, Optional<String> authorizationToken);
+
+    public ResponseEntity<Void> sendEmailPasswordResetToken(Optional<String> email) throws Exception;
+
+    public ResponseEntity<Void> updatePassword(Optional<String> passwordResetToken, PasswordResetRequest request);
+}
