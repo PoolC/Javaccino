8458e0b9000f1b0ef9d128b6b894d745de696160
hcpak <jasotn12@naver.com>
2021-08-03 23:46:10 +0900

    feat: spring security filter 추가

---
+++ src/main/java/com/emotie/api/auth/domain/JwtAuthenticationFilter.java
@@ -0,0 +1,36 @@
+package com.emotie.api.auth.domain;
+
+import com.emotie.api.auth.infra.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.filter.GenericFilterBean;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends GenericFilterBean {
+    private final JwtTokenProvider jwtTokenProvider;
+    private final UserDetailsService userDetailsService;
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        String token = jwtTokenProvider.getToken((HttpServletRequest) request);
+        Optional.ofNullable(token)
+                .map(jwtTokenProvider::getSubject)
+                .map(userDetailsService::loadUserByUsername)
+                .map(userDetails -> new UsernamePasswordAuthenticationToken(userDetails,
+                        "",
+                        userDetails.getAuthorities()))
+                .ifPresent(authentication -> SecurityContextHolder.getContext().setAuthentication(authentication));
+
+        chain.doFilter(request, response);
+    }
+}
---
+++ src/main/java/com/emotie/api/auth/service/MemberDetailService.java
@@ -0,0 +1,22 @@
+package com.emotie.api.auth.service;
+
+import com.emotie.api.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class MemberDetailService implements UserDetailsService {
+    private final MemberRepository memberRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String memberId) throws UsernameNotFoundException {
+        return memberRepository.findById(memberId)
+                .orElseThrow(() -> new UsernameNotFoundException(
+                        String.format("%s 회원은 존재하지 않습니다.", memberId)
+                ));
+    }
+}
