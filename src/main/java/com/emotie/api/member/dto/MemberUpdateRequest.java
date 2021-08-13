b3b9bf5b1978970dd5ac3c9dd063da83625e0fe9
Bucket1572 <sout0811@naver.com>
2021-08-13 17:08:06 +0900

    test: 회원 단위 테스트를 위한 MemberUpdateRequest 클래스 작성

--- src/main/java/com/emotie/api/member/dto/MemberUpdateRequest.java
+++ src/main/java/com/emotie/api/member/dto/MemberUpdateRequest.java
@@ -1,2 +1,36 @@
-package com.emotie.api.member.dto;public class MemberUpdateRequest {
+package com.emotie.api.member.dto;
+
+import com.emotie.api.common.exception.NotSameException;
+import com.emotie.api.member.domain.Gender;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
+
+import java.time.LocalDate;
+
+public class MemberUpdateRequest {
+    private final String password;
+    private final String passwordCheck;
+    private final Gender gender;
+    private final LocalDate dateOfBirth;
+
+    @JsonCreator
+    @Builder
+    public MemberUpdateRequest(
+            @JsonProperty("password") String password,
+            @JsonProperty("passwordCheck") String passwordCheck,
+            @JsonProperty("gender") String gender,
+            @JsonProperty("dateOfBirth") String dateOfBirth
+    ) {
+        this.password = password;
+        this.passwordCheck = passwordCheck;
+        this.gender = Gender.valueOf(gender);
+        this.dateOfBirth = LocalDate.parse(dateOfBirth);
+    }
+
+    private void checkPasswordMatches() {
+        if (!this.password.equals(this.passwordCheck)) {
+            throw new NotSameException("비밀번호와 비밀번호 확인 문자열이 다릅니다.");
+        }
+    }
 }
