2e1ba25e17ed35834568fb0bea85b1ba8451a17a
Bucket1572 <sout0811@naver.com>
2021-08-13 17:36:30 +0900

    test: 회원 정보 수정 단위 테스트 작성

--- src/test/java/com/emotie/api/member/MemberApiTest.java
+++ src/test/java/com/emotie/api/member/MemberApiTest.java
@@ -4,6 +4,7 @@ import com.emotie.api.AcceptanceTest;
 import com.emotie.api.member.domain.Gender;
 import com.emotie.api.member.domain.Member;
 import com.emotie.api.member.dto.MemberCreateRequest;
+import com.emotie.api.member.dto.MemberUpdateRequest;
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
@@ -16,6 +17,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Optional;
 
@@ -26,7 +28,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 @ActiveProfiles("memberDataLoader")
 @TestMethodOrder(MethodOrderer.DisplayName.class)
 public class MemberApiTest extends AcceptanceTest {
-
+    // TODO: 2021-08-13 가입과 수정에 관한 모든 경우에 대하여, password, nickname의 형식이 필요할 것으로 보임.
+    
     /*
         회원가입 테스트를 위한 상수
      */
@@ -39,7 +42,7 @@ public class MemberApiTest extends AcceptanceTest {
         회원가입 테스트
      */
     @Test
-    @DisplayName("테스트 01: 회원가입 실패 [400]; 정보가 하나 이상 누락 됨.")
+    @DisplayName("테스트 01.01.01: 회원가입 실패 [400]; 정보가 하나 이상 누락 됨.")
     public void 회원가입_실패_BAD_REQUEST_1() throws Exception {
         // 회원가입_실패_필요_정보_누락 이 더 좋은 테스트 메소드명 아닌가?
         // given
@@ -60,7 +63,7 @@ public class MemberApiTest extends AcceptanceTest {
     }
 
     @Test
-    @DisplayName("테스트 02: 회원가입 실패 [400]; 선택할 수 없는 성별 값(잘못된 형식)")
+    @DisplayName("테스트 01.01.02: 회원가입 실패 [400]; 선택할 수 없는 성별 값(잘못된 형식)")
     public void 회원가입_실패_BAD_REQUEST_2() throws Exception {
         // given
         MemberCreateRequest request = MemberCreateRequest.builder()
@@ -68,7 +71,7 @@ public class MemberApiTest extends AcceptanceTest {
                 .password(createTestPassword)
                 .passwordCheck(createTestPassword)
                 .gender("Random Gender")
-                .dateOfBirth(LocalDateTime.now().toString())
+                .dateOfBirth(LocalDateTime.now().format(DateTimeFormatter.ISO_DATE))
                 .email(createTestEmail)
                 .build();
 
@@ -88,7 +91,7 @@ public class MemberApiTest extends AcceptanceTest {
                 .password(createTestPassword)
                 .passwordCheck(createTestPassword)
                 .gender(Gender.HIDDEN.toString())
-                .dateOfBirth(LocalDateTime.now().toString())
+                .dateOfBirth(LocalDateTime.now().format(DateTimeFormatter.ISO_DATE))
                 .email("human@earth.com")
                 .build();
 
@@ -100,7 +103,26 @@ public class MemberApiTest extends AcceptanceTest {
     }
 
     @Test
-    @DisplayName("테스트 04: 회원가입 실패 [409]; 이미 사용 중인 닉네임으로 닉네임 설정")
+    @DisplayName("테스트 04: 회원 가입 실패 [400]; 비밀번호가 비밀번호 확인 문자열과 다름")
+    public void 회원가입_실패_BAD_REQUEST_4() throws Exception {
+        MemberCreateRequest request = MemberCreateRequest.builder()
+                .nickname(createTestEmail)
+                .password(createTestPassword)
+                .passwordCheck(MemberDataLoader.wrongPassword)
+                .gender(Gender.HIDDEN.toString())
+                .dateOfBirth(LocalDateTime.now().format(DateTimeFormatter.ISO_DATE))
+                .email(createTestEmail)
+                .build();
+
+        // when
+        ExtractableResponse<Response> response = memberCreateRequest(request);
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+    }
+
+    @Test
+    @DisplayName("테스트 05: 회원가입 실패 [409]; 이미 사용 중인 닉네임으로 닉네임 설정")
     public void 회원가입_실패_CONFLICT_1() throws Exception {
         // given
         MemberCreateRequest request = MemberCreateRequest.builder()
@@ -108,7 +130,7 @@ public class MemberApiTest extends AcceptanceTest {
                 .password(createTestPassword)
                 .passwordCheck(createTestPassword)
                 .gender(Gender.HIDDEN.toString())
-                .dateOfBirth(LocalDateTime.now().toString())
+                .dateOfBirth(LocalDateTime.now().format(DateTimeFormatter.ISO_DATE))
                 .email(createTestEmail)
                 .build();
 
@@ -120,7 +142,7 @@ public class MemberApiTest extends AcceptanceTest {
     }
 
     @Test
-    @DisplayName("테스트 05: 회원가입 실패 [409]; 이미 사용 중인 이메일로 이메일 설정")
+    @DisplayName("테스트 06: 회원가입 실패 [409]; 이미 사용 중인 이메일로 이메일 설정")
     public void 회원가입_실패_CONFLICT_2() throws Exception {
         // given
         MemberCreateRequest request = MemberCreateRequest.builder()
@@ -128,7 +150,7 @@ public class MemberApiTest extends AcceptanceTest {
                 .password(createTestPassword)
                 .passwordCheck(createTestPassword)
                 .gender(Gender.HIDDEN.toString())
-                .dateOfBirth(LocalDateTime.now().toString())
+                .dateOfBirth(LocalDateTime.now().format(DateTimeFormatter.ISO_DATE))
                 .email(MemberDataLoader.authorizedEmail)
                 .build();
 
@@ -140,7 +162,7 @@ public class MemberApiTest extends AcceptanceTest {
     }
 
     @Test
-    @DisplayName("테스트 06: 회원가입 성공 [200]")
+    @DisplayName("테스트 07: 회원가입 성공 [200]")
     public void 회원가입_성공_OK() {
         // given
         MemberCreateRequest request = MemberCreateRequest.builder()
@@ -148,7 +170,7 @@ public class MemberApiTest extends AcceptanceTest {
                 .password(createTestPassword)
                 .passwordCheck(createTestPassword)
                 .gender(Gender.HIDDEN.toString())
-                .dateOfBirth(LocalDateTime.now().toString())
+                .dateOfBirth(LocalDateTime.now().format(DateTimeFormatter.ISO_DATE))
                 .email(createTestEmail)
                 .build();
 
@@ -159,7 +181,75 @@ public class MemberApiTest extends AcceptanceTest {
         assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
     }
 
+    /*
+        회원 정보 수정 테스트
+     */
+    @Test
+    @DisplayName("테스트 08: 회원 정보 수정 실패 [400]; 선택할 수 없는 성별 값(잘못된 형식)")
+    public void 회원정보_수정_실패_BAD_REQUEST_1() throws Exception {
+        // given
+        MemberUpdateRequest request = MemberUpdateRequest.builder()
+                .gender("Random Gender")
+                .build();
 
+        // when
+        ExtractableResponse<Response> response = memberUpdateRequest(request);
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+    }
+
+    @Test
+    @DisplayName("테스트 09: 회원 정보 수정 실패 [400]; 비밀번호와 비밀번호 문자열이 다름")
+    public void 회원정보_수정_실패_BAD_REQUEST_2() throws Exception {
+        // given
+        MemberUpdateRequest request = MemberUpdateRequest.builder()
+                .password(createTestPassword)
+                .passwordCheck(MemberDataLoader.wrongPassword)
+                .build();
+
+        // when
+        ExtractableResponse<Response> response = memberUpdateRequest(request);
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+    }
+
+    @Test
+    @DisplayName("테스트 10: 회원 정보 수정 성공 [200]; 모든 정보를 주었을 때")
+    public void 회원정보_수정_성공_200_OK_1() {
+        // given
+        MemberUpdateRequest request = MemberUpdateRequest.builder()
+                .password(createTestPassword)
+                .passwordCheck(createTestPassword)
+                .gender(Gender.HIDDEN.toString())
+                .dateOfBirth(LocalDateTime.now().format(DateTimeFormatter.ISO_DATE))
+                .build();
+
+        // when
+        ExtractableResponse<Response> response = memberUpdateRequest(request);
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+    }
+
+    @Test
+    @DisplayName("테스트 11: 회원 정보 수정 성공 [200]; 일부 정보만 주었을 때")
+    public void 회원정보_수정_성공_200_OK_2() {
+        // given
+        MemberUpdateRequest request = MemberUpdateRequest.builder()
+                .password(createTestPassword)
+                .passwordCheck(createTestPassword)
+                .build();
+
+        // when
+        ExtractableResponse<Response> response = memberUpdateRequest(request);
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+    }
+
+    // TODO: 2021-08-13 아무런 정보 수정이 없을 때에도 200 OK가 반환되는지 여부?
 
     private static ExtractableResponse<Response> memberCreateRequest(MemberCreateRequest request) {
         return RestAssured
@@ -167,6 +257,16 @@ public class MemberApiTest extends AcceptanceTest {
                 .body(request)
                 .contentType(APPLICATION_JSON_VALUE)
                 .when().post("/members")
+                .then().log().all()
+                .extract();
+    }
+
+    private static ExtractableResponse<Response> memberUpdateRequest(MemberUpdateRequest request) {
+        return RestAssured
+                .given().log().all()
+                .body(request)
+                .contentType(APPLICATION_JSON_VALUE)
+                .when().put("/members")
                 .then().log().all()
                 .extract();
     }
