f7a8dfd1af58c4bda2d4e183dc530279c05f313a
Bucket1572 <sout0811@naver.com>
2021-08-19 16:05:32 +0900

    Test: 팔로우 로직 제외 모든 테스트 통과

--- src/test/java/com/emotie/api/member/MemberApiTest.java
+++ src/test/java/com/emotie/api/member/MemberApiTest.java
@@ -1,42 +1,42 @@
 package com.emotie.api.member;
 
 import com.emotie.api.AcceptanceTest;
-import com.emotie.api.auth.infra.JwtTokenProvider;
+import com.emotie.api.auth.dto.LoginRequest;
+import com.emotie.api.auth.dto.LoginResponse;
 import com.emotie.api.member.domain.Gender;
-import com.emotie.api.member.domain.Member;
-import com.emotie.api.member.domain.MemberRole;
 import com.emotie.api.member.dto.MemberCreateRequest;
+import com.emotie.api.member.dto.MemberFollowResponse;
 import com.emotie.api.member.dto.MemberUpdateRequest;
-import com.emotie.api.member.repository.FolloweesRepository;
-import com.emotie.api.member.repository.FollowersRepository;
-import com.emotie.api.member.repository.MemberRepository;
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import lombok.RequiredArgsConstructor;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.List;
-import java.util.Optional;
 
-import static com.emotie.api.auth.AuthAcceptanceTest.authorizedLogin;
-import static com.emotie.api.auth.AuthAcceptanceTest.unauthorizedLogin;
+import static com.emotie.api.auth.AuthAcceptanceTest.*;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.not;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @ActiveProfiles("memberDataLoader")
 @TestMethodOrder(MethodOrderer.DisplayName.class)
 @RequiredArgsConstructor
-// @Transactional 필요한가?
+@Transactional
 public class MemberApiTest extends AcceptanceTest {
     // TODO: 2021-08-13 가입과 수정에 관한 모든 경우에 대하여, password, nickname의 형식이 필요할 것으로 보임.
 
 //    // 나중에 실제 구현할 때 추가해야할 테스트 코드
 //    private final MemberRepository memberRepository;
+//    private final PasswordHashProvider passwordHashProvider;
 //    private final FollowersRepository followersRepository;
 //    private final FolloweesRepository followeesRepository;
 //    private final JwtTokenProvider jwtTokenProvider;
@@ -47,6 +47,7 @@ public class MemberApiTest extends AcceptanceTest {
     private static final String emptySeq = "",
             createTestEmail = "randomhuman@gmail.com",
             createTestPassword = "creative!password",
+            changedPassword = "better_password?",
             notExistNickname = "공릉동익룡";
 
 //    @BeforeEach
@@ -247,7 +248,7 @@ public class MemberApiTest extends AcceptanceTest {
         // given
         String accessToken = authorizedLogin();
         MemberUpdateRequest request = MemberUpdateRequest.builder()
-                .password(createTestPassword)
+                .password(changedPassword)
                 .passwordCheck(MemberDataLoader.wrongPassword)
                 .build();
 
@@ -264,8 +265,8 @@ public class MemberApiTest extends AcceptanceTest {
         // given
         String accessToken = "";
         MemberUpdateRequest request = MemberUpdateRequest.builder()
-                .password(createTestPassword)
-                .passwordCheck(createTestPassword)
+                .password(changedPassword)
+                .passwordCheck(changedPassword)
                 .build();
 
         // when
@@ -282,8 +283,8 @@ public class MemberApiTest extends AcceptanceTest {
         String now = LocalDateTime.now().format(DateTimeFormatter.ISO_DATE);
         String accessToken = authorizedLogin();
         MemberUpdateRequest request = MemberUpdateRequest.builder()
-                .password(createTestPassword)
-                .passwordCheck(createTestPassword)
+                .password(changedPassword)
+                .passwordCheck(changedPassword)
                 .gender(Gender.HIDDEN.toString())
                 .dateOfBirth(now)
                 .build();
@@ -306,10 +307,10 @@ public class MemberApiTest extends AcceptanceTest {
     @DisplayName("테스트 13: 회원 정보 수정 성공 [200]; 일부 정보만 주었을 때")
     public void 회원정보_수정_성공_OK_2() throws Exception {
         // given
-        String accessToken = authorizedLogin();
+        String accessToken = changedAuthorizedLogin();
         MemberUpdateRequest request = MemberUpdateRequest.builder()
-                .password(createTestPassword)
-                .passwordCheck(createTestPassword)
+                .password(MemberDataLoader.password)
+                .passwordCheck(MemberDataLoader.password)
                 .build();
 
         // when
@@ -342,10 +343,23 @@ public class MemberApiTest extends AcceptanceTest {
     }
 
     @Test
-    @DisplayName("테스트 15: 회원 팔로우 실패 [404]; 해당 nickname의 회원이 존재하지 않음")
-    public void 회원_팔로우_실패_NOT_FOUND() throws Exception {
+    @DisplayName("테스트 15: 회원 팔로우 실패 [403]; 이메일 인증하지  않음")
+    public void 회원_팔로우_실패_FORBIDDEN() throws Exception {
         // given
         String accessToken = unauthorizedLogin();
+
+        // when
+        ExtractableResponse<Response> response = memberFollowRequest(accessToken, MemberDataLoader.authorizedEmail);
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.FORBIDDEN.value());
+    }
+
+    @Test
+    @DisplayName("테스트 16: 회원 팔로우 실패 [404]; 해당 nickname의 회원이 존재하지 않음")
+    public void 회원_팔로우_실패_NOT_FOUND() throws Exception {
+        // given
+        String accessToken = authorizedLogin();
 
         // when
         ExtractableResponse<Response> response = memberFollowRequest(accessToken, notExistNickname);
@@ -355,8 +369,21 @@ public class MemberApiTest extends AcceptanceTest {
     }
 
     @Test
-    @DisplayName("테스트 16: 회원 팔로우 성공 [200]; Unfollowed -> Following")
-    public void 회원_팔로우_성공_OK_1() throws Exception {
+    @DisplayName("테스트 17: 회원 팔로우 실패 [409]; 해당 nickname의 회원이 팔로우 신청할 수 없는 대상일 때")
+    public void 회원_팔로우_실패_CONFLICT_1() throws Exception {
+        // given
+        String accessToken = authorizedLogin();
+
+        // when
+        ExtractableResponse<Response> response = memberFollowRequest(accessToken, MemberDataLoader.unauthorizedEmail);
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.CONFLICT.value());
+    }
+
+    @Test
+    @DisplayName("테스트 18: 회원 팔로우 실패 [409]; 해당 nickname의 회원이 자신일 때")
+    public void 회원_팔로우_실패_CONFLICT_2() throws Exception {
         // given
         String accessToken = authorizedLogin();
 
@@ -364,8 +391,22 @@ public class MemberApiTest extends AcceptanceTest {
         ExtractableResponse<Response> response = memberFollowRequest(accessToken, MemberDataLoader.authorizedEmail);
 
         // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.CONFLICT.value());
+    }
+
+    @Test
+    @DisplayName("테스트 19: 회원 팔로우 성공 [200]; Unfollowed -> Following")
+    public void 회원_팔로우_성공_OK_1() throws Exception {
+        // given
+        String accessToken = authorizedLogin();
+
+        // when
+        ExtractableResponse<Response> response = memberFollowRequest(accessToken, MemberDataLoader.followerEmail);
+
+        // then
         assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
-        assertThat(response.body()).hasFieldOrPropertyWithValue("isFollowing", true);
+        assertThat(response.body().as(MemberFollowResponse.class))
+                .hasFieldOrPropertyWithValue("isFollowing", true);
 
 //        // 실제 팔로잉하고 있는지 확인하는 부분
 //        Member followed = getByEmailAssertingExistence(MemberDataLoader.authorizedEmail);
@@ -384,19 +425,20 @@ public class MemberApiTest extends AcceptanceTest {
     }
 
     @Test
-    @DisplayName("테스트 17: 회원 언팔로우 성공 [200]; Followed -> Unfollowing")
+    @DisplayName("테스트 20: 회원 언팔로우 성공 [200]; Followed -> Unfollowing")
     public void 회원_팔로우_성공_OK_2() throws Exception {
         // given
         String accessToken = authorizedLogin();
         // 팔로우 상태에서
-        memberFollowRequest(accessToken, MemberDataLoader.authorizedEmail);
+        memberFollowRequest(accessToken, MemberDataLoader.followerEmail);
 
         // when
-        ExtractableResponse<Response> response = memberFollowRequest(accessToken, MemberDataLoader.authorizedEmail);
+        ExtractableResponse<Response> response = memberFollowRequest(accessToken, MemberDataLoader.followerEmail);
 
         // then
         assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
-        assertThat(response.body()).hasFieldOrPropertyWithValue("isFollowing", false);
+        assertThat(response.body().as(MemberFollowResponse.class))
+                .hasFieldOrPropertyWithValue("isFollowing", false);
 
 //        // 실제 언팔로우 했는지 확인하는 부분
 //        Member unfollowed = getByEmailAssertingExistence(MemberDataLoader.authorizedEmail);
@@ -418,7 +460,7 @@ public class MemberApiTest extends AcceptanceTest {
         회원 탈퇴 테스트
      */
     @Test
-    @DisplayName("테스트 18: 회원 탈퇴 실패 [401]; 로그인하지 않음.")
+    @DisplayName("테스트 21: 회원 탈퇴 실패 [401]; 로그인하지 않음.")
     public void 회원_탈퇴_실패_UNAUTHORIZED() throws Exception {
         // given
         String accessToken = "";
@@ -431,7 +473,7 @@ public class MemberApiTest extends AcceptanceTest {
     }
 
     @Test
-    @DisplayName("테스트 19: 회원 탈퇴 실패 [403]; 본인도 아니고, 관리자도 아님")
+    @DisplayName("테스트 22: 회원 탈퇴 실패 [403]; 본인도 아니고, 관리자도 아님")
     public void 회원_탈퇴_실패_FORBIDDEN() throws Exception {
         // given
         String accessToken = unauthorizedLogin();
@@ -444,20 +486,20 @@ public class MemberApiTest extends AcceptanceTest {
     }
 
     @Test
-    @DisplayName("테스트 20: 회원 탈퇴 실패 [404]; 해당하는 회원이 없음")
+    @DisplayName("테스트 23: 회원 탈퇴 실패 [404]; 해당하는 회원이 없음")
     public void 회원_탈퇴_실패_NOT_FOUND() throws Exception {
         // given
         String accessToken = authorizedLogin();
 
         // when
-        ExtractableResponse<Response> response = memberWithdrawalRequest(accessToken, emptySeq);
+        ExtractableResponse<Response> response = memberWithdrawalRequest(accessToken, notExistNickname);
 
         // then
         assertThat(response.statusCode()).isEqualTo(HttpStatus.NOT_FOUND.value());
     }
 
     @Test
-    @DisplayName("테스트 21: 회원 탈퇴 성공 [200]; 본인일 때")
+    @DisplayName("테스트 24: 회원 탈퇴 성공 [200]; 본인일 때")
     public void 회원_탈퇴_성공_OK_1() throws Exception {
         // given
         String accessToken = unauthorizedLogin();
@@ -474,10 +516,10 @@ public class MemberApiTest extends AcceptanceTest {
     }
 
     @Test
-    @DisplayName("테스트 22: 회원 탈퇴 성공 [200]; 관리자일 때")
+    @DisplayName("테스트 25: 회원 탈퇴 성공 [200]; 관리자일 때")
     public void 회원_탈퇴_성공_OK_2() throws Exception {
         // given
-        String accessToken = authorizedLogin();
+        String accessToken = adminLogin();
 
         // when
         ExtractableResponse<Response> response = memberWithdrawalRequest(accessToken, MemberDataLoader.unauthorizedEmail);
@@ -529,6 +571,28 @@ public class MemberApiTest extends AcceptanceTest {
                 .when().delete("/members/{nickname}", nickname)
                 .then().log().all()
                 .extract();
+    }
+
+    private static String changedAuthorizedLogin() {
+        LoginRequest request = LoginRequest.builder()
+                .email(MemberDataLoader.authorizedEmail)
+                .password(changedPassword)
+                .build();
+
+        return loginRequest(request)
+                .as(LoginResponse.class)
+                .getAccessToken();
+    }
+
+    private static String adminLogin() {
+        LoginRequest request = LoginRequest.builder()
+                .email(MemberDataLoader.adminEmail)
+                .password(MemberDataLoader.password)
+                .build();
+
+        return loginRequest(request)
+                .as(LoginResponse.class)
+                .getAccessToken();
     }
 
 //    private Member getByAccessTokenAssertingExistence(String accessToken) throws Exception {
