81cff0f70bff3fa302cd8c11d1440de743291e78
Mardwe <powerkim417@naver.com>
2021-08-20 03:07:07 +0900

    create, update DTO 누락된 것 다시 업로드

--- src/main/java/com/emotie/api/member/dto/MemberUpdateRequest.java
+++ src/main/java/com/emotie/api/member/dto/MemberUpdateRequest.java
@@ -1,2 +1,31 @@
-package com.emotie.api.member.dto;public class MemberUpdateRequest {
+package com.emotie.api.member.dto;
+
+import com.emotie.api.common.exception.NotSameException;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDate;
+
+@Getter
+public class MemberUpdateRequest {
+
+    private final String password;
+    private final String passwordCheck;
+    private final String gender;
+    private final LocalDate dateOfBirth;
+
+    @JsonCreator
+    @Builder
+    public MemberUpdateRequest(
+            @JsonProperty("password") String password,
+            @JsonProperty("passwordCheck") String passwordCheck,
+            @JsonProperty("gender") String gender,
+            @JsonProperty("dateOfBirth") String dateOfBirth) {
+        this.password = password;
+        this.passwordCheck = passwordCheck;
+        this.gender = gender;
+        this.dateOfBirth = LocalDate.parse(dateOfBirth);
+    }
 }
