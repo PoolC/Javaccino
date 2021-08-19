207617493cb4eab87ecbac75561fea66bf4969dd
Mardwe <powerkim417@naver.com>
2021-08-19 19:05:53 +0900

    Member 컨트롤러 작성

---
+++ src/main/java/com/emotie/api/member/controller/MemberController.java
@@ -0,0 +1,43 @@
+package com.emotie.api.member.controller;
+
+import com.emotie.api.member.domain.Member;
+import com.emotie.api.member.dto.MemberCreateRequest;
+import com.emotie.api.member.dto.MemberFollowResponse;
+import com.emotie.api.member.dto.MemberUpdateRequest;
+import com.emotie.api.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/members")
+@RequiredArgsConstructor
+public class MemberController {
+    private final MemberService memberService;
+
+    @PostMapping
+    public ResponseEntity<Void> memberCreate(@RequestBody MemberCreateRequest request){
+        memberService.memberCreate(request);
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping
+    public ResponseEntity<Void> memberUpdate(@RequestBody MemberUpdateRequest request){
+        memberService.memberUpdate(request);
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/follow/{nickname}")
+    public ResponseEntity<MemberFollowResponse> followToggle(@AuthenticationPrincipal Member member, @PathVariable String nickname){
+        Boolean isFollowing = memberService.followToggle(member, nickname);
+        return ResponseEntity.ok(new MemberFollowResponse(isFollowing));
+    }
+
+    @DeleteMapping("/{nickname}")
+    public ResponseEntity<Void> memberWithdrawal(@AuthenticationPrincipal Member member, @PathVariable String nickname){
+        memberService.memberWithdrawal(member, nickname);
+        return ResponseEntity.ok().build();
+    }
+
+}
