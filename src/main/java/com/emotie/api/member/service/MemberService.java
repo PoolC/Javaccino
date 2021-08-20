d922c12a1d2955232f7e116a2d3787a83b6dfe2c
Mardwe <powerkim417@naver.com>
2021-08-20 14:23:44 +0900

    Member 서비스 메인 함수들 구현

--- src/main/java/com/emotie/api/member/service/MemberService.java
+++ src/main/java/com/emotie/api/member/service/MemberService.java
@@ -1,9 +1,21 @@
 package com.emotie.api.member.service;
 
 import com.emotie.api.member.domain.Member;
+import com.emotie.api.member.dto.MemberCreateRequest;
+import com.emotie.api.member.dto.MemberUpdateRequest;
 
 public interface MemberService {
+    public void memberCreate(MemberCreateRequest request);
+
+    public void memberUpdate(Member member, MemberUpdateRequest request);
+
+    public Boolean followToggle(Member member, String nickname);
+
+    public void memberWithdrawal(Member member, String nickname);
+
     public Member getMemberByEmail(String email);
 
     public Member getMemberIfRegistered(String email, String password);
+
+    public Member getMemberByNickname(String nickname);
 }
