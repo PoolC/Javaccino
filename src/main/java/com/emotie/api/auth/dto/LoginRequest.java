9ba4a95403f1aec154fa6600d03b8bf3c971d86e
hcpak <jasotn12@naver.com>
2021-08-21 18:36:42 +0900

    feat: 요청이 제대로 왔는지 검증 로직 추가

--- src/main/java/com/emotie/api/auth/dto/LoginRequest.java
+++ src/main/java/com/emotie/api/auth/dto/LoginRequest.java
@@ -5,9 +5,14 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Getter;
 
+import javax.validation.constraints.NotNull;
+
 @Getter
 public class LoginRequest {
+    @NotNull(message = "이메일을 입력해주세요.")
     private final String email;
+
+    @NotNull(message = "비밀번호를 입력해주세요.")
     private final String password;
 
     @JsonCreator
