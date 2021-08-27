719cf4fd942cc3b5f9e98b45db9c454ea3495884
Bucket1572 <sout0811@naver.com>
2021-08-27 15:33:07 +0900

    Feat: Emotion Enum 클래스 도메인

--- src/main/java/com/emotie/api/diaries/domain/Emotion.java
+++ src/main/java/com/emotie/api/diaries/domain/Emotion.java
@@ -1,2 +1,5 @@
-package com.emotie.api.diaries.domain;public enum EMOTION {
+package com.emotie.api.diaries.domain;
+
+public enum Emotion {
+    FLUTTER, JEALOUS, SURPRISED, ANGRY, HAPPY, SAD, TIRED, NONE
 }
