822add3a26e0ee21cbd56367d3c23d19b5ec58a6
Mardwe <powerkim417@naver.com>
2021-08-30 04:02:51 +0900

    Test: 방명록 DataLoader 추가

--- src/test/java/com/emotie/api/guestbook/GuestbookDataLoader.java
+++ src/test/java/com/emotie/api/guestbook/GuestbookDataLoader.java
@@ -1,5 +1,9 @@
 package com.emotie.api.guestbook;
 
+import com.emotie.api.guestbook.domain.Guestbook;
+import com.emotie.api.guestbook.repository.GuestbookRepository;
+import com.emotie.api.member.MemberDataLoader;
+import com.emotie.api.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Profile;
@@ -9,10 +13,25 @@ import org.springframework.stereotype.Component;
 @Profile("guestbookDataLoader")
 @RequiredArgsConstructor
 public class GuestbookDataLoader implements CommandLineRunner {
-    
+    private final GuestbookRepository guestbookRepository;
+
+    private static MemberRepository memberRepository;
+
+    public static String unauthorizedUUID = memberRepository.findByNickname(MemberDataLoader.unauthorizedEmail).get().getUUID(),
+            authorizedUUID = memberRepository.findByNickname(MemberDataLoader.authorizedEmail).get().getUUID();
+    public static Integer existId = 1, notExistId = 2;
 
     @Override
     public void run(String... args) throws Exception {
-
+        // TODO: UUID는 String인데, ownerId와 guestId는 int 협의 필요
+        guestbookRepository.save(
+                // unauth 유저가 auth 유저에게 남긴 방명록
+                Guestbook.builder()
+                        .id(existId)
+//                        .ownerId(unauthorizedUUID)
+//                        .guestId(authorizedUUID)
+                        .content("구독하고 갑니다~~")
+                        .reportCount(0)
+                        .build());
     }
 }
