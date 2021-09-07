0c72f045bf48a7c8ab4f506a40fc0b4f59b09344
Bucket1572 <sout0811@naver.com>
2021-09-07 22:27:42 +0900

    Logic: 감정 점수 로직 구현; 가중치 0.9;

--- src/main/java/com/emotie/api/diaries/vo/EmotionStatus.java
+++ src/main/java/com/emotie/api/diaries/vo/EmotionStatus.java
@@ -4,11 +4,20 @@ import lombok.Getter;
 
 @Getter
 public class EmotionStatus {
-    private final double score;
-    private final int count;
+    private static final double TRANSFER_WEIGHT = 0.9;
+    private double score;
+    private int count;
 
     public EmotionStatus(double score, int count) {
         this.score = score;
         this.count = count;
+    }
+
+    public void addOne() {
+        this.count += 1;
+    }
+
+    public void updateScore(double newScore) {
+        this.score = TRANSFER_WEIGHT * this.score + (1 - TRANSFER_WEIGHT) * newScore;
     }
 }
