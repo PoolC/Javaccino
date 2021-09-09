f0bdfeec87b56ee5f14c2b460d67b4ae269740d2
Bucket1572 <sout0811@naver.com>
2021-09-09 14:45:30 +0900

    Logic: 기존에 있었던 toMap 관련 메소드 모두 제거 및 형태 수정(?)

--- src/main/java/com/emotie/api/diaries/domain/Diaries.java
+++ src/main/java/com/emotie/api/diaries/domain/Diaries.java
@@ -1,7 +1,6 @@
 package com.emotie.api.diaries.domain;
 
 import com.emotie.api.common.domain.Postings;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,66 +13,26 @@ import java.util.*;
 @NoArgsConstructor
 @Entity(name = "emodiaries")
 public class Diaries extends Postings {
-    @Column(name = "emotion_tag_id", nullable = false)
-    private Integer emotionTagId;
+    @Column(name = "emotion", nullable = false)
+    private Emotion emotion;
 
     @Column(name = "is_opened", nullable = false)
     private Boolean isOpened;
 
     @Builder
     public Diaries(
-            Integer memberId, String content, Integer emotionTagId, Boolean isOpened, Integer reportCount
+            Integer writerId, String content, Emotion emotion, Boolean isOpened
     ) {
-        this.writerId = memberId;
+        this.writerId = writerId;
         this.content = content;
-        this.emotionTagId = emotionTagId;
+        this.emotion = emotion;
         this.isOpened = isOpened;
-        this.reportCount = reportCount;
-    }
-
-    /**
-     * 다이어리 인스턴스의 모든 데이터를 Map으로 추출함.
-     *
-     * @return 인스턴스의 데이터가 들어있는 Map
-     */
-    private Map<String, Object> toMap(){
-        return Map.ofEntries(
-                Map.entry("id", this.id),
-                Map.entry("member_id", this.writerId),
-                Map.entry("content", this.content),
-                Map.entry("emotion_tag_id", this.emotionTagId),
-                Map.entry("is_opened", this.isOpened),
-                Map.entry("report_count", this.reportCount)
-        );
+        this.reportCount = 0;
     }
 
     @Override
     public Postings readPosting() {
         return this;
-    }
-
-    /**
-     * 새로운 내용으로 업데이트를 진행하고 기존 내용을 Map으로 반환함.
-     *
-     * @param content -> 새로운 내용
-     * @param emotionTagId -> 새로운 감정 태그
-     * @param isOpened -> 접근성 갱신
-     * @return 기존 내용
-     */
-    public Map<String, Object> updatePosting(String content, Integer emotionTagId, Boolean isOpened) {
-        Map<String, Object> prevPostingData = this.toMap();
-        this.content = content;
-        this.emotionTagId = emotionTagId;
-        this.isOpened = isOpened;
-        return prevPostingData;
-    }
-
-    public List<Map<String, Object>> deletePostings(List<Diaries> diaries) {
-        List<Map<String, Object>> prevPostingData = new LinkedList<Map<String, Object>>();
-        for (Diaries diary : diaries) {
-            prevPostingData.add(diary.toMap());
-        }
-        return prevPostingData;
     }
 
     @Override
