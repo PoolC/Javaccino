cf218f44af74d98a939510142cde2ec38c81382f
Bucket1572 <sout0811@naver.com>
2021-09-15 19:25:01 +0900

    Logic: @Getter 추가

--- src/main/java/com/emotie/api/diaries/dto/DiaryUpdateRequest.java
+++ src/main/java/com/emotie/api/diaries/dto/DiaryUpdateRequest.java
@@ -4,12 +4,14 @@ import com.emotie.api.diaries.domain.Emotion;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
+import lombok.Getter;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.PastOrPresent;
 import java.time.LocalDate;
 
+@Getter
 public class DiaryUpdateRequest {
     @NotNull
     @PastOrPresent(message = "죄송합니다. 시간 여행을 기록하실 수는 없습니다.")
