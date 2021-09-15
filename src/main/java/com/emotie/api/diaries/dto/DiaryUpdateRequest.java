939e53c1cb6a2210e504999f0362eba33af8484f
Bucket1572 <sout0811@naver.com>
2021-09-15 18:48:03 +0900

    DTO: 다이어리 업데이트 리퀘스트 작성

--- src/main/java/com/emotie/api/diaries/dto/DiaryUpdateRequest.java
+++ src/main/java/com/emotie/api/diaries/dto/DiaryUpdateRequest.java
@@ -1,11 +1,40 @@
 package com.emotie.api.diaries.dto;
 
+import com.emotie.api.diaries.domain.Emotion;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PastOrPresent;
+import java.time.LocalDate;
+
 public class DiaryUpdateRequest {
+    @NotNull
+    @PastOrPresent(message = "죄송합니다. 시간 여행을 기록하실 수는 없습니다.")
+    private final LocalDate issuedDate;
+
+    @NotNull
+    private final Emotion emotion;
+
+    @NotBlank
+    private final String content;
+
+    @NotNull
+    private final Boolean isOpened;
 
     @Builder
-    public DiaryUpdateRequest() {
-
+    @JsonCreator
+    public DiaryUpdateRequest(
+            @JsonProperty(value = "issued_date", required = true) LocalDate issuedDate,
+            @JsonProperty(value = "emotion", required = true) Emotion emotion,
+            @JsonProperty(value = "content", required = true) String content,
+            @JsonProperty(value = "is_opened", required = true) Boolean isOpened
+    ) {
+        this.issuedDate = issuedDate;
+        this.emotion = emotion;
+        this.content = content;
+        this.isOpened = isOpened;
     }
 }
