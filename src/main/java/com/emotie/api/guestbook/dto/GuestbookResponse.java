f23dffba10066644a3b450cf620d937ff83a6f7a
Mardwe <powerkim417@naver.com>
2021-09-16 03:09:00 +0900

    Refactor: response 필드 객체->string으로 변경

--- src/main/java/com/emotie/api/guestbook/dto/GuestbookResponse.java
+++ src/main/java/com/emotie/api/guestbook/dto/GuestbookResponse.java
@@ -10,8 +10,8 @@ import java.time.LocalDateTime;
 @Getter
 public class GuestbookResponse {
     private final Integer id;
-    private final Member owner;
-    private final Member writer;
+    private final String ownerId;
+    private final String writerId;
     private final String content;
     private final Integer reportCount;
     private final LocalDateTime createdAt;
@@ -20,11 +20,12 @@ public class GuestbookResponse {
     @JsonCreator
     public GuestbookResponse(Guestbook guestbook) {
         this.id = guestbook.getId();
-        this.owner = guestbook.getOwner();
-        this.writer = guestbook.getWriter();
+        this.ownerId = guestbook.getOwner().getUUID();
+        this.writerId = guestbook.getWriter().getUUID();
         this.content = guestbook.getContent();
         this.reportCount = guestbook.getReportCount();
         this.createdAt = guestbook.getCreatedAt();
         this.updatedAt = guestbook.getUpdateAt();
     }
+
 }
--- src/test/java/com/emotie/api/guestbook/GuestbookDataLoader.java
+++ src/test/java/com/emotie/api/guestbook/GuestbookDataLoader.java
@@ -14,6 +14,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 import java.util.UUID;
 
