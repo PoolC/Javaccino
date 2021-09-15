aac1df85f7fab6ca52b02a5d73073aa8b5ed7e6c
Mardwe <powerkim417@naver.com>
2021-09-16 01:39:54 +0900

    Refactor: 방명록의 멤버들을 객체로 넘겨줌에 따른 서비스 코드 수정

--- src/main/java/com/emotie/api/guestbook/service/GuestbookService.java
+++ src/main/java/com/emotie/api/guestbook/service/GuestbookService.java
@@ -2,6 +2,7 @@ package com.emotie.api.guestbook.service;
 
 import com.emotie.api.auth.exception.UnauthorizedException;
 import com.emotie.api.guestbook.domain.Guestbook;
+import com.emotie.api.guestbook.domain.MemberReportGuestbook;
 import com.emotie.api.guestbook.dto.GuestbookCreateRequest;
 import com.emotie.api.guestbook.dto.GuestbookUpdateRequest;
 import com.emotie.api.guestbook.exception.MyselfException;
@@ -11,6 +12,7 @@ import com.emotie.api.member.repository.MemberRepository;
 import com.emotie.api.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.NoSuchElementException;
@@ -24,24 +26,26 @@ public class GuestbookService {
 
     public List<Guestbook> getAllBoards(Member user, String nickname) {
         checkGetAllBoardsRequestValidity(user, nickname);
-        String ownerId = memberService.getMemberByNickname(nickname).getUUID();
-        return guestbookRepository.findByOwnerId(ownerId);
+        Member owner = memberService.getMemberByNickname(nickname);
+        return guestbookRepository.findByOwner(owner);
     }
 
     public void create(Member user, GuestbookCreateRequest request, String nickname) {
-        checkCreateRequestValidity(user, nickname);
+        checkCreateRequestValidity(user, request, nickname);
+        Member owner = memberService.getMemberByNickname(nickname);
         guestbookRepository.save(
                 Guestbook.builder()
-                        .ownerId(memberService.getMemberByNickname(nickname).getUUID())
-                        .writerId(user.getUUID())
+                        .owner(owner)
+                        .writer(user)
                         .content(request.getContent())
                         .reportCount(0)
+                        .isGlobalBlinded(false)
                         .build()
         );
     }
 
     public void update(Member user, GuestbookUpdateRequest request, Integer guestbookId) {
-        checkUpdateRequestValidity(user, guestbookId);
+        checkUpdateRequestValidity(user, request, guestbookId);
         Guestbook guestbook = getGuestbookById(guestbookId);
         guestbook.update(request);
         guestbookRepository.saveAndFlush(guestbook);
@@ -52,17 +56,10 @@ public class GuestbookService {
 
         Guestbook target = getGuestbookById(guestbookId);
 
-        if (user.isReported(target)) {
-            unreport(user, target);
-            memberRepository.saveAndFlush(user);
-            guestbookRepository.saveAndFlush(target);
-            return false;
-        }
-
-        report(user, target);
+        report(user.isReported(target), user, target);
         memberRepository.saveAndFlush(user);
         guestbookRepository.saveAndFlush(target);
-        return true;
+        return user.isReported((target));
     }
 
     public void delete(Member executor, Integer guestbookId) {
@@ -72,18 +69,18 @@ public class GuestbookService {
 
     public void clear(Member user, String nickname) {
         checkClearRequestValidity(user, nickname);
-        guestbookRepository.deleteByOwnerId(nickname);
+        Member owner = memberService.getMemberByNickname(nickname);
+        guestbookRepository.deleteByOwner(owner);
     }
 
-    public Boolean toggleBlind(Member user, Integer guestbookId) {
-        checkBlindRequestValidity(user, guestbookId);
-
-        Guestbook target = getGuestbookById(guestbookId);
-
-        // TODO: 글 숨기기 논의 해결 후
-        return true;
-//        if (user.isBlinded(target)) {
-//            unblind(user, target);
+//    public Boolean toggleGlobalBlind(Member user, Integer guestbookId) {
+//        checkGlobalBlindRequestValidity(user, guestbookId);
+//
+//        Guestbook target = getGuestbookById(guestbookId);
+//
+//        return true;
+//        if (user.hasGlobalBlinded(target)) {
+//            unGlobalBlind(user, target);
 //            memberRepository.saveAndFlush(user);
 //            guestbookRepository.saveAndFlush(target);
 //            return false;
@@ -93,43 +90,76 @@ public class GuestbookService {
 //        memberRepository.saveAndFlush(user);
 //        guestbookRepository.saveAndFlush(target);
 //        return true;
-    }
+//    }
+//
+//    public Boolean toggleLocalBlind(Member user, Integer guestbookId) {
+//        checkGlobalBlindRequestValidity(user, guestbookId);
+//
+//        Guestbook target = getGuestbookById(guestbookId);
+//
+//        if (user.isGlobalBlinded(target)) {
+//            unGlobalBlind(user, target);
+//            memberRepository.saveAndFlush(user);
+//            guestbookRepository.saveAndFlush(target);
+//            return false;
+//        }
+//
+//        blind(user, target);
+//        memberRepository.saveAndFlush(user);
+//        guestbookRepository.saveAndFlush(target);
+//        return true;
+//    }
 
     /*
     유효성 검사 메서드
+    (content null 여부는 Request 검증에서 체크)
      */
     private void checkGetAllBoardsRequestValidity(Member user, String nickname) {
         memberService.checkLogin(user);
+        checkNicknameExists(nickname);
     }
 
-    private void checkCreateRequestValidity(Member user, String nickname) {
+    private void checkCreateRequestValidity(Member user, GuestbookCreateRequest request, String nickname) {
         memberService.checkLogin(user);
+        checkNicknameExists(nickname);
         checkNotOwner(user, nickname);
-        memberService.getMemberByNickname(nickname);
     }
 
-    private void checkUpdateRequestValidity(Member user, Integer guestbookId) {
+    private void checkUpdateRequestValidity(Member user, GuestbookUpdateRequest request, Integer guestbookId) {
         memberService.checkLogin(user);
+        checkGuestbookIdExists(guestbookId);
         checkWriter(user, guestbookId);
+        checkNotOverReported(guestbookId);
     }
 
     private void checkToggleReportRequestValidity(Member user, Integer guestbookId) {
         memberService.checkLogin(user);
+        checkGuestbookIdExists(guestbookId);
         checkNotWriter(user, guestbookId);
     }
 
     private void checkDeleteRequestValidity(Member user, Integer guestbookId) {
+        memberService.checkLogin(user);
+        checkGuestbookIdExists(guestbookId);
         checkWriterOrOwner(user, guestbookId);
-        checkNotReported(guestbookId);
+        checkNotOverReported(guestbookId);
     }
 
     private void checkClearRequestValidity(Member user, String nickname) {
+        memberService.checkLogin(user);
+        checkNicknameExists(nickname);
         checkOwner(user, nickname);
-        memberService.getMemberByNickname(nickname);
     }
 
-    private void checkBlindRequestValidity(Member user, Integer guestbookId) {
+    private void checkGlobalBlindRequestValidity(Member user, Integer guestbookId) {
+        memberService.checkLogin(user);
+        checkGuestbookIdExists(guestbookId);
         checkOwner(user, guestbookId);
+    }
+
+    private void checkLocalBlindRequestValidity(Member user, Integer guestbookId) {
+        memberService.checkLogin(user);
+        checkGuestbookIdExists(guestbookId);
     }
 
     /*
@@ -141,61 +171,56 @@ public class GuestbookService {
         });
     }
 
-    private Member getWriterByGuestbook(Guestbook guestbook) {
-        return memberService.getMemberById(guestbook.getWriterId());
+    // TODO: MemberService.isNicknameExists 가 같은 역할을 하는 것 같은데 통일해야 함
+    private void checkNicknameExists(String nickname){
+        if (!memberRepository.existsByNickname(nickname)){
+            throw new NoSuchElementException("해당 nickname을 가진 사용자가 없습니다.");
+        }
     }
 
-    private Member getOwnerByGuestbook(Guestbook guestbook) {
-        return memberService.getMemberById(guestbook.getOwnerId());
+    private void checkGuestbookIdExists(Integer guestbookId){
+        if (!guestbookRepository.existsById(guestbookId)){
+            throw new NoSuchElementException("해당 nickname을 가진 사용자가 없습니다.");
+        }
     }
 
     // TODO: 자기자신을 팔로우할 수 없는 CannotFollowException과 합칠 수 있을까?
     private void checkNotOwner(Member user, String nickname) {
-        if (user.equals(memberService.getMemberByNickname(nickname))) {
+        Member owner = memberService.getMemberByNickname(nickname);
+        if (user.equals(owner)) {
             throw new MyselfException("자신의 방명록에는 글을 쓸 수 없습니다.");
         }
     }
 
     private void checkWriter(Member user, Integer guestbookId) {
-        Member writer = getWriterByGuestbook(getGuestbookById(guestbookId));
+        Member writer = getGuestbookById(guestbookId).getWriter();
         if (!user.equals(writer)) {
             throw new UnauthorizedException("해당 방명록 글의 작성자가 아닙니다.");
         }
     }
 
     private void checkNotWriter(Member user, Integer guestbookId) {
-        Member writer = getWriterByGuestbook(getGuestbookById(guestbookId));
+        Member writer = getGuestbookById(guestbookId).getWriter();
         if (user.equals(writer)) {
             throw new MyselfException("자신이 작성한 방명록 글은 신고할 수 없습니다.");
         }
     }
 
-    // TODO: 글 숨기기 논의 해결 후
-    // TODO: 트랜잭션 안 겹치게
-    private void report(Member user, Guestbook target) {
-        user.report(target);
-        target.reportedBy(user);
-        getWriterByGuestbook(target).updateReportCount(true);
-    }
-
-    // TODO: 글 숨기기 논의 해결 후
-    private void unreport(Member user, Guestbook target) {
-        user.unreport(target);
-        target.unreportedBy(user);
-        getWriterByGuestbook(target).updateReportCount(false);
+    private void report(Boolean isReported, Member user, Guestbook target) {
+        user.report(isReported, new MemberReportGuestbook(user, target));
+        target.reportedBy(isReported, new MemberReportGuestbook(user, target));
+        target.getOwner().updateReportCount(isReported);
+        // report cnt 꼬이지 않게 잘 보기
     }
 
     private void checkWriterOrOwner(Member user, Integer guestbookId) {
         Guestbook guestbook = getGuestbookById(guestbookId);
-        Member writer = getWriterByGuestbook(guestbook);
-        Member owner = getOwnerByGuestbook(guestbook);
-        if (!(user.equals(writer) || user.equals(owner))) {
+        if (!(user.equals(guestbook.getWriter()) || user.equals(guestbook.getOwner()))) {
             throw new UnauthorizedException("방명록 게시물을 삭제할 권한이 없습니다.");
         }
     }
 
-    // TODO: blinded 칼럼을 추가해서 관리하는게 더 나을까?
-    private void checkNotReported(Integer guestbookId) {
+    private void checkNotOverReported(Integer guestbookId) {
         Guestbook guestbook = getGuestbookById(guestbookId);
         if (guestbook.isNotOverReported()) {
             throw new UnauthorizedException("신고를 많이 받아 삭제할 수 없는 방명록입니다.");
@@ -209,23 +234,20 @@ public class GuestbookService {
         }
     }
 
-    // TODO: 트랜잭션 안 겹치게
-    // TODO: 글 숨기기 논의 해결 후
-    private void blind(Member user, Guestbook target) {
-//        user.blind(target);
-//        target.blindedBy(user);
-    }
-
-    // TODO: 글 숨기기 논의 해결 후
-    private void unblind(Member user, Guestbook target) {
-//        user.unblind(target);
-//        target.unblindedBy(user);
-    }
-
     private void checkOwner(Member user, Integer guestbookId) {
-        Member owner = getOwnerByGuestbook(getGuestbookById(guestbookId));
+        Member owner = getGuestbookById(guestbookId).getOwner();
         if (!user.equals(owner)) {
             throw new UnauthorizedException("방명록 게시물을 숨길 권한이 없습니다.");
         }
+    }
+
+    private void blind(Member user, Guestbook target) {
+        user.blind(target);
+        target.blindedBy(user);
+    }
+
+    private void unblind(Member user, Guestbook target) {
+        user.unblind(target);
+        target.unblindedBy(user);
     }
 }
