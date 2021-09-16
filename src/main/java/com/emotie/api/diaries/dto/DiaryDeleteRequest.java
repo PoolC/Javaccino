01e9741740316092399238e6877af5b1bf9f01ca
Bucket1572 <sout0811@naver.com>
2021-09-16 22:05:07 +0900

    Feat: DTO; diary delete request 작성

--- src/main/java/com/emotie/api/diaries/dto/DiaryDeleteRequest.java
+++ src/main/java/com/emotie/api/diaries/dto/DiaryDeleteRequest.java
@@ -1,17 +1,22 @@
 package com.emotie.api.diaries.dto;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Getter;
+
+import java.util.List;
 
 @Getter
 public class DiaryDeleteRequest {
 
-
+    private final List<Integer> id;
 
     @Builder
     @JsonCreator
-    public DiaryDeleteRequest() {
-
+    public DiaryDeleteRequest(
+            @JsonProperty(value = "id", required = true) List<Integer> id
+    ) {
+        this.id = id;
     }
 }
