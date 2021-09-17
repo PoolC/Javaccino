9bbd3dcaa57df1ab3c21f7253f6f70ff179c57b7
hcpak <jasotn12@naver.com>
2021-09-17 12:41:30 +0900

    feat: auth api 명세 변경으로 인한 로직 변경 - 로그인시 토큰 만료시간이 지났을 때 http status 401로 보내도록 변경

--- src/main/java/com/emotie/api/auth/domain/JwtAuthenticationFilter.java
+++ src/main/java/com/emotie/api/auth/domain/JwtAuthenticationFilter.java
@@ -1,5 +1,6 @@
 package com.emotie.api.auth.domain;
 
+import com.emotie.api.auth.exception.UnauthenticatedException;
 import com.emotie.api.auth.infra.JwtTokenProvider;
 import com.emotie.api.member.domain.Member;
 import com.emotie.api.member.domain.MemberRole;
@@ -15,7 +16,9 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Collections;
 import java.util.Optional;
 
 @RequiredArgsConstructor
@@ -24,20 +27,26 @@ public class JwtAuthenticationFilter extends GenericFilterBean {
     private final UserDetailsService userDetailsService;
 
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException, UnauthenticatedException {
         String token = jwtTokenProvider.getToken((HttpServletRequest) request);
-        Optional.ofNullable(token)
-                .map(jwtTokenProvider::getSubject)
-                .map(userDetailsService::loadUserByUsername)
-                .map(userDetails -> new UsernamePasswordAuthenticationToken(userDetails,
-                        "",
-                        userDetails.getAuthorities()))
-                .ifPresentOrElse(authentication -> SecurityContextHolder.getContext().setAuthentication(authentication), () -> {
-                    Member publicMember = new Member(MemberRoles.getDefaultFor(MemberRole.PUBLIC));
-                    UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(publicMember, "", publicMember.getAuthorities());
-                    SecurityContextHolder.getContext().setAuthentication(authentication);
-                });
+        try {
+            Optional.ofNullable(token)
+                    .map(jwtTokenProvider::getSubject)
+                    .map(userDetailsService::loadUserByUsername)
+                    .map(userDetails -> new UsernamePasswordAuthenticationToken(userDetails,
+                            "",
+                            userDetails.getAuthorities()))
+                    .ifPresentOrElse(authentication -> SecurityContextHolder.getContext().setAuthentication(authentication), () -> {
+                        Member publicMember = new Member(MemberRoles.getDefaultFor(MemberRole.PUBLIC));
+                        UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(publicMember, "", publicMember.getAuthorities());
+                        SecurityContextHolder.getContext().setAuthentication(authentication);
+                    });
+            chain.doFilter(request, response);
+        } catch (UnauthenticatedException e) {
+            HttpServletResponse res = (HttpServletResponse) response;
+            res.sendError(HttpServletResponse.SC_UNAUTHORIZED, Collections.singletonMap("message", "토큰이 만료됐거나 잘못됐습니다. 다시 로그인해주세요.").toString());
+        }
 
-        chain.doFilter(request, response);
+
     }
 }
