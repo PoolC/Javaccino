22575571ced2dbbc1450b3d264cfa4c2aece6a88
Mardwe <powerkim417@naver.com>
2021-09-20 03:05:55 +0900

    Test: 방명록 id를 임의삽입하지 않고 save한뒤 받아오는 식으로 수정; 신고자 인스턴스 및 신고 동작 추가

--- src/test/java/com/emotie/api/guestbook/GuestbookDataLoader.java
+++ src/test/java/com/emotie/api/guestbook/GuestbookDataLoader.java
@@ -3,7 +3,11 @@ package com.emotie.api.guestbook;
 import com.emotie.api.auth.infra.PasswordHashProvider;
 import com.emotie.api.common.domain.Postings;
 import com.emotie.api.guestbook.domain.Guestbook;
+import com.emotie.api.guestbook.domain.MemberReportGuestbook;
+import com.emotie.api.guestbook.dto.GuestbookCreateRequest;
 import com.emotie.api.guestbook.repository.GuestbookRepository;
+import com.emotie.api.guestbook.service.GuestbookService;
+import com.emotie.api.member.MemberDataLoader;
 import com.emotie.api.member.domain.Gender;
 import com.emotie.api.member.domain.Member;
 import com.emotie.api.member.domain.MemberRole;
@@ -24,78 +28,123 @@ public class GuestbookDataLoader implements CommandLineRunner {
     private final GuestbookRepository guestbookRepository;
     private final PasswordHashProvider passwordHashProvider;
     private final MemberRepository memberRepository;
+    private final GuestbookService guestbookService;
 
     public static String
-            writerEmail = "writer@naver.com",
-            ownerEmail = "owner@naver.com",
+            writerEmail = "writer@emotie.com",
+            ownerEmail = "owner@emotie.com",
+            reporterEmail = "reporter@emotie.com",
             writerNickname = "Writer Kim",
             ownerNickname = "Owner Park",
+            reporterNickname = "reporter no. ",
             notExistNickname = "Not exist nickname",
             guestbookPassword = "qhdks1!",
             introduction = "We are Guestbook test users",
             createContent = "구독하고 갑ㄴ디ㅏ",
             changedContent = "구독하고 갑니다";
 
-    public static Integer existId = 1, overReportedId = 2, notExistId = -1;
+    public static Integer existId, almostReportedId, overReportedId, notExistId = -1;
+    public static Member writer, owner;
+    public static Member[] reporters = new Member[Guestbook.reportCountThreshold];
 
     @Override
     public void run(String... args) throws Exception {
-        // 방명록 작성자
-        memberRepository.save(
-                Member.builder()
-                        .UUID(UUID.randomUUID().toString())
-                        .email(writerEmail)
-                        .nickname(writerNickname)
-                        .passwordHash(passwordHashProvider.encodePassword(guestbookPassword))
-                        .gender(Gender.HIDDEN)
-                        .dateOfBirth(LocalDate.now())
-                        .introduction(introduction)
-                        .passwordResetToken(null)
-                        .passwordResetTokenValidUntil(null)
-                        .authorizationToken(null)
-                        .authorizationTokenValidUntil(null)
-                        .reportCount(0)
-                        .roles(MemberRoles.getDefaultFor(MemberRole.MEMBER))
-                        .build());
-        // 방명록 주인장
-        memberRepository.save(
-                Member.builder()
-                        .UUID(UUID.randomUUID().toString())
-                        .email(ownerEmail)
-                        .nickname(ownerNickname)
-                        .passwordHash(passwordHashProvider.encodePassword(guestbookPassword))
-                        .gender(Gender.HIDDEN)
-                        .dateOfBirth(LocalDate.now())
-                        .introduction(introduction)
-                        .passwordResetToken(null)
-                        .passwordResetTokenValidUntil(null)
-                        .authorizationToken(null)
-                        .authorizationTokenValidUntil(null)
-                        .reportCount(0)
-                        .roles(MemberRoles.getDefaultFor(MemberRole.MEMBER))
-                        .build());
+        /*
+        Member 생성
+         */
+        writer = Member.builder()
+                .UUID(UUID.randomUUID().toString())
+                .email(writerEmail)
+                .nickname(writerNickname)
+                .passwordHash(passwordHashProvider.encodePassword(guestbookPassword))
+                .gender(Gender.HIDDEN)
+                .dateOfBirth(LocalDate.now())
+                .introduction(introduction)
+                .passwordResetToken(null)
+                .passwordResetTokenValidUntil(null)
+                .authorizationToken(null)
+                .authorizationTokenValidUntil(null)
+                .reportCount(0)
+                .roles(MemberRoles.getDefaultFor(MemberRole.MEMBER))
+                .build();
+        memberRepository.save(writer);
 
-        Member owner = memberRepository.findByNickname(ownerNickname).get();
-        Member writer = memberRepository.findByNickname(writerNickname).get();
+        owner = Member.builder()
+                .UUID(UUID.randomUUID().toString())
+                .email(ownerEmail)
+                .nickname(ownerNickname)
+                .passwordHash(passwordHashProvider.encodePassword(guestbookPassword))
+                .gender(Gender.HIDDEN)
+                .dateOfBirth(LocalDate.now())
+                .introduction(introduction)
+                .passwordResetToken(null)
+                .passwordResetTokenValidUntil(null)
+                .authorizationToken(null)
+                .authorizationTokenValidUntil(null)
+                .reportCount(0)
+                .roles(MemberRoles.getDefaultFor(MemberRole.MEMBER))
+                .build();
+        memberRepository.save(owner);
 
-        guestbookRepository.save(
+        // 신고자
+        for (int i=0 ; i<Guestbook.reportCountThreshold ; i++){
+            reporters[i] = Member.builder()
+                    .UUID(UUID.randomUUID().toString())
+                    .email(i + reporterEmail)
+                    .nickname(reporterNickname + i)
+                    .passwordHash(passwordHashProvider.encodePassword(guestbookPassword))
+                    .gender(Gender.HIDDEN)
+                    .dateOfBirth(LocalDate.now())
+                    .introduction(introduction)
+                    .passwordResetToken(null)
+                    .passwordResetTokenValidUntil(null)
+                    .authorizationToken(null)
+                    .authorizationTokenValidUntil(null)
+                    .reportCount(0)
+                    .roles(MemberRoles.getDefaultFor(MemberRole.MEMBER))
+                    .build();
+            memberRepository.save(reporters[i]);
+        }
+
+        /*
+        방명록
+         */
+
+        // 일반 방명록
+        existId = guestbookRepository.save(
                 Guestbook.builder()
-                        .id(existId)
+                .owner(owner)
+                .writer(writer)
+                .content("구독하고 갑니다~~")
+                .reportCount(0)
+                .isGlobalBlinded(false)
+                .build()).getId();
+        guestbookService.toggleReport(owner, existId);
+
+        // 신고 과다 직전 방명록
+        almostReportedId = guestbookRepository.save(
+                Guestbook.builder()
                         .owner(owner)
                         .writer(writer)
-                        .content("구독하고 갑니다~~")
+                        .content("신고 직전의 게시물")
                         .reportCount(0)
                         .isGlobalBlinded(false)
-                        .build());
-        guestbookRepository.save(
-                // 신고된 방명록
+                        .build()).getId();
+        for (int i=0 ; i<Guestbook.reportCountThreshold-1 ; i++){
+            guestbookService.toggleReport(reporters[i], almostReportedId);
+        }
+
+        // 신고 과다 방명록
+        overReportedId = guestbookRepository.save(
                 Guestbook.builder()
-                        .id(overReportedId)
                         .owner(owner)
                         .writer(writer)
-                        .content("무수한 신고의 요청이..!")
-                        .reportCount(Postings.reportCountThreshold)
+                        .content("신고가 너무 많아서 숨겨지게 될 방명록")
+                        .reportCount(0)
                         .isGlobalBlinded(false)
-                        .build());
+                        .build()).getId();
+        for (int i=0 ; i<Guestbook.reportCountThreshold ; i++){
+            guestbookService.toggleReport(reporters[i], overReportedId);
+        }
     }
 }
