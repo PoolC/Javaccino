e1e24ddd906c70b859eb9455461f9e4371d5165f
Mardwe <powerkim417@naver.com>
2021-09-20 03:04:27 +0900

    Test: activeprofiles "test" 추가

--- src/test/java/com/emotie/api/AcceptanceTest.java
+++ src/test/java/com/emotie/api/AcceptanceTest.java
@@ -5,7 +5,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class AcceptanceTest {
 
