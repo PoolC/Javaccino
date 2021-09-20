81818050a7da5ece620778b8a7d9dadb1fe1085b
Mardwe <powerkim417@naver.com>
2021-09-21 01:46:06 +0900

    Refactor: 불필요한 어노테이션 제거

--- src/main/java/com/emotie/api/guestbook/domain/MemberLocalBlindGuestbook.java
+++ src/main/java/com/emotie/api/guestbook/domain/MemberLocalBlindGuestbook.java
@@ -4,8 +4,6 @@ import com.emotie.api.member.domain.Member;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.NotFound;
-import org.hibernate.annotations.NotFoundAction;
 
 import javax.persistence.*;
 
@@ -20,13 +18,11 @@ public class MemberLocalBlindGuestbook {
     @GeneratedValue
     private Long id;
 
-    //    @NotFound(action = NotFoundAction.IGNORE)
-    @ManyToOne(cascade = CascadeType.REMOVE)
+    @ManyToOne
     @JoinColumn(name = "member_id")
     private Member member;
 
-    //    @NotFound(action = NotFoundAction.IGNORE)
-    @ManyToOne(cascade = CascadeType.REMOVE)
+    @ManyToOne
     @JoinColumn(name = "guestbook_id")
     private Guestbook guestbook;
 
--- src/main/java/com/emotie/api/guestbook/domain/MemberReportGuestbook.java
+++ src/main/java/com/emotie/api/guestbook/domain/MemberReportGuestbook.java
@@ -4,8 +4,6 @@ import com.emotie.api.member.domain.Member;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.NotFound;
-import org.hibernate.annotations.NotFoundAction;
 
 import javax.persistence.*;
 
@@ -20,12 +18,10 @@ public class MemberReportGuestbook {
     @GeneratedValue
     private Long id;
 
-    //    @NotFound(action = NotFoundAction.IGNORE)
     @ManyToOne
     @JoinColumn(name = "member_id")
     private Member member;
 
-    //    @NotFound(action = NotFoundAction.IGNORE)
     @ManyToOne
     @JoinColumn(name = "guestbook_id")
     private Guestbook guestbook;
