dd9552c1a07dba944be26f2fdb847c9e2b968b1a
Mardwe <powerkim417@naver.com>
2021-09-21 23:48:21 +0900

    Feat: 조건에 따른 방명록 리스트 불러오는 쿼리 추가

--- src/main/java/com/emotie/api/guestbook/repository/GuestbookRepository.java
+++ src/main/java/com/emotie/api/guestbook/repository/GuestbookRepository.java
@@ -3,6 +3,8 @@ package com.emotie.api.guestbook.repository;
 import com.emotie.api.guestbook.domain.Guestbook;
 import com.emotie.api.member.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
@@ -13,6 +15,12 @@ public interface GuestbookRepository extends JpaRepository<Guestbook, Integer> {
     List<Guestbook> findByOwner(Member owner);
 
     Optional<Guestbook> findById(Integer guestbookId);
+
+    @Query(value = "SELECT g FROM guestbooks g WHERE g.owner = :owner AND g.reportCount < :reportCountThreshold AND g.isGlobalBlinded = false")
+    List<Guestbook> findForUserByOwner(@Param("owner") Member owner, @Param("reportCountThreshold") Integer reportCountThreshold);
+
+    @Query(value = "SELECT g FROM guestbooks g WHERE g.owner = :owner AND g.reportCount < :reportCountThreshold")
+    List<Guestbook> findForOwnerByOwner(@Param("owner") Member owner, @Param("reportCountThreshold") Integer reportCountThreshold);
 
     @Transactional
     void deleteAllByOwner(Member owner);
