acd4bc14f9de71173c117a682bb7d384e97a27d3
Mardwe <powerkim417@naver.com>
2021-10-01 18:01:23 +0900

    Refactor: 명세에 맞게 필드 수정

--- src/main/java/com/emotie/api/guestbook/dto/GuestbookResponse.java
+++ src/main/java/com/emotie/api/guestbook/dto/GuestbookResponse.java
@@ -11,36 +11,30 @@ import java.time.LocalDateTime;
 @Getter
 public class GuestbookResponse {
     private Long id;
-    private String ownerId;
     private String writerId;
     private String content;
     private Integer reportCount;
     private LocalDateTime createdAt;
-    private LocalDateTime updatedAt;
 
     public static GuestbookResponse of(Guestbook guestbook) {
         return GuestbookResponse.builder()
                 .id(guestbook.getId())
-                .ownerId(guestbook.getOwner().getUUID())
                 .writerId(guestbook.getWriter().getUUID())
                 .content(guestbook.getContent())
                 .reportCount(guestbook.getReportCount())
                 .createdAt(guestbook.getCreatedAt())
-                .updatedAt(guestbook.getUpdateAt())
                 .build();
     }
 
     @JsonCreator
     @Builder
-    public GuestbookResponse(@JsonProperty("id") Long id, @JsonProperty("ownerId") String ownerId, @JsonProperty("writerId") String writerId,
+    public GuestbookResponse(@JsonProperty("id") Long id, @JsonProperty("writerId") String writerId,
                              @JsonProperty("content") String content, @JsonProperty("reportCount") Integer reportCount,
-                             @JsonProperty("createdAt") LocalDateTime createdAt, @JsonProperty("updatedAt") LocalDateTime updatedAt) {
+                             @JsonProperty("createdAt") LocalDateTime createdAt) {
         this.id = id;
-        this.ownerId = ownerId;
         this.writerId = writerId;
         this.content = content;
         this.reportCount = reportCount;
         this.createdAt = createdAt;
-        this.updatedAt = updatedAt;
     }
 }
